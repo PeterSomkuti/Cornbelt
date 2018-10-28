d35cf84453c8123f27ae1b04b6fba78a402c40e2
PeterSomkuti <ps345@le.ac.uk>
2018-10-28 21:25:29 +0100

    Started writing manuscript

--- analysis.py
+++ analysis.py
@@ -1,15 +1,27 @@
+from matplotlib import rcParams
+rcParams['font.family'] = 'serif'
+rcParams['font.serif'] = ['Times', 'Times New Roman']
+
+rcParams['xtick.labelsize'] = 7
+rcParams['ytick.labelsize'] = 7
+rcParams['ytick.labelsize'] = 7
+rcParams['axes.labelsize'] = 8
+rcParams['mathtext.fontset'] = 'stix'
+
+from matplotlib import pyplot as plt
+
 import h5py
 import numpy as np
 import pandas as pd
-from netCDF4 import num2date
+from netCDF4 import num2date, Dataset
 
-from matplotlib import rcParams
-rcParams['font.family'] = 'serif'
-rcParams['font.sans-serif'] = ['Times New Roman']
+from scipy.interpolate import RegularGridInterpolator as RGI
+import itertools
 
-from matplotlib import pyplot as plt
 import palettable as pt
 import statsmodels.formula.api as smf
+
+from bbox import *
 
 ###############################################################################
 ## Change analysis parameters here
@@ -17,15 +29,15 @@ import statsmodels.formula.api as smf
 
 resample = 'MS'
 rs_apply = 'mean'
-min_count = 50
+min_sif = 0.0
+min_count = 200
 min_date = '20100101'
 max_date = '20161231'
 anomaly_stat = 'median'
 
 ###############################################################################
+######### PREPPING DATA #######################################################
 
-
-######### PREPPING DATA ###########
 
 ### Read in the USDA data
 
@@ -49,52 +61,96 @@ usda_production = pd.read_csv('corn_soy_wheat_production.csv', sep=',', thousand
 h5 = h5py.File('fluorescence_subset.h5', 'r')
 dates = num2date(h5['frac_days_since'][:], units='days since 1970-01-01')
 
-qual = (h5['qual_flag'][:] == 1)
-
 # Create dataframe using the dates, and then read all 1-dim data arrays into it
-df = pd.DataFrame(index=dates[qual])
+df = pd.DataFrame(index=dates)
 
+print("Reading HDF")
 for key in h5.keys():
     if len(h5[key].shape) == 1:
         if h5[key].shape[0] == len(h5['Exposure_id']):
             # Don't want the empty 'simple' fields
             if 'simple' in key: continue
             
-            print(f"Reading {key}")
-            df[key] = h5[key][:][qual]
-
+            #print(f"Reading {key}")
+            df[key] = h5[key][:] #[qual]
+print("Done")
 # Here are the keys for the SIF data fields
-abs_755_list = [x for x in df.columns if
-                  (('corr' in x) and ('abs' in x) and ('755' in x))]
-abs_772_list = [x for x in df.columns if
-                  (('corr' in x) and ('abs' in x) and ('772' in x))]
-abs_755_list_P = [x for x in df.columns if
-                  (('corr' in x) and ('abs' in x) and ('755_P' in x))]
-abs_755_list_S = [x for x in df.columns if
-                  (('corr' in x) and ('abs' in x) and ('755_S' in x))]
-abs_772_list_P = [x for x in df.columns if
-                  (('corr' in x) and ('abs' in x) and ('772_P' in x))]
-abs_772_list_S = [x for x in df.columns if
-                  (('corr' in x) and ('abs' in x) and ('772_S' in x))]
+
+SL = dict() # Sif key list
+for absrel, pol, window in itertools.product(
+        ['abs', 'rel'], ['P', 'S', ''], ['755', '772']):
+    if pol == '':
+        polstr = ''
+    else:
+        polstr = f'_{pol}'
+
+    dkey = f"{absrel}_{window}{polstr}"
+
+    SL[dkey] = [x for x in df.columns if
+                (('corr' in x) and (absrel in x) and (f'{window}{polstr}' in x))]
+
 
 # Scale SIF to units of W/m2/sr/nm
-df.loc[:, abs_755_list] *= (1e7 / 755)**2
-df.loc[:, abs_772_list] *= (1e7 / 772)**2
+df.loc[:, SL['abs_755']] *= (1e7 / 755)**2
+df.loc[:, SL['abs_772']] *= (1e7 / 772)**2
 
-# Get CASA fluxes from gC/m2/s to gC/m2/yr
-df.loc[:, ['CASA_GEE', 'CASA_NEE']] *= 60*60*24*365
+# DPsurf filter? Does not seem to do anything significant
+#dpfilt = (df['r_svsv_psurf_772'] - df['r_svap_psurf_772']) < -10000
+#df.loc[dpfilt, :] = np.nan
+
+# Get CASA fluxes from kgC/m2/s to gC/m2/yr
+df.loc[:, ['CASA_GEE', 'CASA_NEE']] *= 60*60*24*365*1000
 
 # Set bad SM values to NaNs
 sm_list = ['sm_combined', 'sm_active', 'sm_passive']
 for sm in sm_list:
-    neg_sm = df.loc[:, sm] < 0
-    df.loc[neg_sm, sm] = np.nan 
+    bad_sm = df.loc[:, sm+'_uncrt'] <= 0
+    df.loc[bad_sm, sm] = np.nan 
 
 
 
 # Make Gome-2 use only overlapping data
 g2_ov = df.loc[:, 'gome2_overlap'] < 0.9
 df.loc[g2_ov, 'gome2_sif'] = np.nan
+
+# Read UoE fluxes and put in dataframe
+df['uoe-7.1b'] = np.nan
+
+nc = Dataset('u2ol_7.1b_fluxmap_uoe_r2012_1x1.nc', 'r')
+#nc = Dataset('u72ol_fluxmap_uoe_1x1.nc', 'r')
+
+uoe_lats = nc.variables['lat'][:]
+uoe_lons = nc.variables['lon'][:]
+
+date_array = nc.variables['start_date'][:]
+dates_uoe = pd.to_datetime(['{:d}-{:02d}-{:02d}'.format(x[0], x[1], x[2])
+                            for x in date_array])
+
+lon1_idx = np.argmin(np.abs(uoe_lons - lon_min))
+lon2_idx = np.argmin(np.abs(uoe_lons - lon_max))
+lat1_idx = np.argmin(np.abs(uoe_lats - lat_min))
+lat2_idx = np.argmin(np.abs(uoe_lats - lat_max))
+
+df_flux = pd.DataFrame(index=dates_uoe, columns=['uoe-7.1b'])
+
+flux = nc.variables['flux'][:,:,:] # these come as kgC/s/m2
+flux *= 86400 * 1e3 # this is now in gC/day
+
+for year in range(2009, 2017):
+    for month in range(1,13):
+ 
+        df_select = ((df.index.year == year) &
+                     (df.index.month == month))
+        uoe_idx = np.where((dates_uoe.year == year) &
+                           (dates_uoe.month == month))[0][0]
+
+        gridint = RGI((uoe_lats, uoe_lons), flux[uoe_idx])
+
+        df.loc[df_select, 'uoe-7.1b'] = gridint(np.dstack([df.loc[df_select, 'lat'].values,
+                                                           df.loc[df_select, 'lon'].values])[0])
+
+
+
 
 # Resample the data according to resample period
 dfs = df[min_date:max_date].resample(resample).apply(rs_apply)
@@ -103,22 +159,34 @@ dfs['count'] = df[min_date:max_date].loc[:, 'Exposure_id'].resample(resample).co
 # And drop all rows which are below the minimum count threshold
 dfs.loc[dfs['count'] < min_count, :] = np.nan
 
+for c in SL['abs_755']+SL['abs_772']:
+    too_small = dfs.loc[:, c] < min_sif
+    dfs.loc[too_small, c] = np.nan
 
-# Normalise sif to planted area?
+
+# Normalise sif to planted area? Does not significantly change anything..
 for year in np.unique(dfs.index.year):
     year_mask = dfs.index.year == year
-    dfs.loc[year_mask, abs_755_list] *= all_planted_area_rel[year]
+#    dfs.loc[year_mask, abs_755_list] *= all_planted_area_rel[year]
 
 
+
+dfs.loc[:, 'uoe-7.1b'] *= dfs.index.daysinmonth * 12
+#df_flux.loc[:, 'uoe-7.1b'] = np.mean(flux, axis=(1,2)) * df_flux.index.daysinmonth # / 1e12 # TgC
+
+#dfs['uoe-7.1b'] = df_flux[min_date:max_date].loc[:, 'uoe-7.1b']
+    
 # Create anomaly dataframe
 dfa = dfs.copy()
 
 # Anomalies for monthlies
-for m, dfg in dfa.groupby(dfa.index.month):
-    dfa[dfa.index.month == m] -= dfg.apply(anomaly_stat)
+for (m,d), dfg in dfa.groupby([dfa.index.month,
+                              dfa.index.day]):
+    dfa[(dfa.index.month == m) & (dfa.index.day == d)] -= dfg.apply(anomaly_stat)
 
 # GRACE is already an anomaly, so we put that data back in
 dfa.loc[:, 'GRACE'][:] = dfs.loc[:, 'GRACE'][:]
+dfa.loc[:, 'spei'][:] = dfs.loc[:, 'spei'][:]
 dfa.loc[:, 'count'][:] = dfs.loc[:, 'count'][:]
 
 ##### Make a nice overview plot, showing the SIF anomalies in the context of
@@ -133,50 +201,60 @@ def align_yaxis(ax1, v1, ax2, v2):
     ax2.set_ylim(miny+dy, maxy+dy)
     
 def ts_overview(plot_df, sif_list, aux_list,
+                figsize=(5, 6),
                 aux_labels=None,
+                plot_sif=True,
                 fname='ts_grid.png',
                 ncol=1, hline=0.0, symm_lim=False):
 
-    nrow = int(np.ceil((len(aux_list) + 2 )/ ncol))
+    sif_count = 0
+    if plot_sif:
+        sif_count = 2
+        
+    nrow = int(np.ceil((len(aux_list) + sif_count)/ ncol))
 
-    fig, axarr = plt.subplots(nrow, ncol, figsize=(8, 8), dpi=300)
+    fig, axarr = plt.subplots(nrow, ncol, figsize=figsize, dpi=300)
     axlist = axarr.flatten()
-    
-    # Make SIF time series as first entry
-    sifcolor='g'
-    ax = axlist[0]
+
     sif_data = plot_df.loc[:, sif_list]
     sif_lim = np.max(np.abs(sif_data.median(axis=1))) * 1.3
-    # Plot median of all SIF data
-    ax.plot(sif_data.median(axis=1), color=sifcolor,
-            label='SIF [W/m$^2$/sr/nm]')
-    ax.legend(fontsize=7, loc='lower left', framealpha=1.0)
-    # Standard deviation to spot where significant anomalies lie
-    if hline is not None:
-        ax.hlines([-sif_data.median(axis=1).std(),
-                   sif_data.median(axis=1).std()],
-                  xmin=sif_data.index[0], xmax=sif_data.index[-1],
-                  linestyle='dashed', color='g', lw=1.0)
+    sifcolor='g'
+    
+    if plot_sif:
+        # Make SIF time series as first entry
+        ax = axlist[0]
 
-    # Fill area between min and max of all SIF data
-    ax.fill_between(x=plot_df.index,
-                    y1=sif_data.loc[:, sif_list].min(axis=1),
-                    y2=sif_data.loc[:, sif_list].max(axis=1),
-                    color=sifcolor,
-                    alpha=0.25)
-    if symm_lim:
-        ax.set_ylim(-sif_lim, sif_lim)
-    if hline != None:
-        ax.hlines(hline, plot_df.index[0], plot_df.index[-1],
-                  colors='grey', linestyles='dashed',
-                  linewidth=0.5)
-    ax.set_xticklabels([])
+        # Plot median of all SIF data
+        ax.plot(sif_data.median(axis=1), color=sifcolor,
+                label='SIF [W/m$^2$/sr/nm]')
 
-    # Plot the number of measurements
-    ax = axlist[1]
-    ax.plot(plot_df['count'], label='GOSAT count', color='k')
-    ax.legend(fontsize=7, loc='lower left', framealpha=1.0)
-    ax.set_xticklabels([])
+        ax.legend(fontsize=7, loc='lower left', framealpha=1.0)
+        # Standard deviation to spot where significant anomalies lie
+        if hline is not None:
+            ax.hlines([-sif_data.median(axis=1).std(),
+                       sif_data.median(axis=1).std()],
+                      xmin=sif_data.index[0], xmax=sif_data.index[-1],
+                      linestyle='dashed', color='g', lw=1.0)
+
+        # Fill area between min and max of all SIF data
+        ax.fill_between(x=plot_df.index,
+                        y1=sif_data.loc[:, sif_list].min(axis=1),
+                        y2=sif_data.loc[:, sif_list].max(axis=1),
+                        color=sifcolor,
+                        alpha=0.25)
+        if symm_lim:
+            ax.set_ylim(-sif_lim, sif_lim)
+        if hline != None:
+            ax.hlines(hline, plot_df.index[0], plot_df.index[-1],
+                      colors='grey', linestyles='dashed',
+                      linewidth=0.5)
+        ax.set_xticklabels([])
+
+        # Plot the number of measurements
+        ax = axlist[1]
+        ax.plot(plot_df['count'], label='GOSAT count', color='k')
+        ax.legend(fontsize=7, loc='lower left', framealpha=1.0)
+        ax.set_xticklabels([])
 
     # Then fill the rest in with other data
     aux_colors = pt.tableau.BlueRed_12.mpl_colors
@@ -186,7 +264,7 @@ def ts_overview(plot_df, sif_list, aux_list,
 
         aux_lim = np.max(np.abs(plot_df.loc[:, aux])) * 1.3
         
-        ax = axlist[i+2]
+        ax = axlist[i+sif_count]
 
         if aux_labels == None:
             aux_label = aux
@@ -196,8 +274,13 @@ def ts_overview(plot_df, sif_list, aux_list,
         ax.plot(plot_df.loc[:, aux],
                 #color=aux_colors[i],
                 label=aux_label)
+        
         if symm_lim:
             ax.set_ylim(-aux_lim, aux_lim)
+            if ('GPP' in aux_label) or ('NPP' in aux_label):
+                ax.invert_yaxis()
+
+            
         #ax.set_title(aux)
         ax_handles, ax_labels = ax.get_legend_handles_labels()
         
@@ -233,35 +316,101 @@ def ts_overview(plot_df, sif_list, aux_list,
     
 ###############################################################################
 
-
-
 # Data prep finished - now make some nice plots and calculations
 
+# First, plot the SIF time series, along with anomalies and measurement count
+for pol in ['_P', '_S', '']:
+    fig, axarr = plt.subplots(2, 1, figsize=(4, 2.5))
 
-aux_list = ['gome2_sif', 'myd13_evi',
-            #'albedo_1',
-            'trmm_precip', 'GRACE',
-            'myd11_lst_day', 'sm_combined',
-            'fvc', 'lai', 'CASA_GEE', 'CASA_NEE']
-aux_labels = ['GOME-2 SIF [W/m$^2$/sr/nm]',
+    sl = axarr[0].plot(dfs.loc[:, SL[f'abs_755{pol}']].median(axis=1), 'g-')
+    axarr[0].fill_between(dfs.index,
+                          y1=dfs.loc[:, SL[f'abs_755{pol}']].min(axis=1),
+                          y2=dfs.loc[:, SL[f'abs_755{pol}']].max(axis=1),
+                          color='green', alpha=0.15)
+
+    axarr[0].set_ylabel('SIF\n[mW/m$\mathrm{^2}$/sr/nm]')
+    axarr[0].set_xticklabels([])
+
+    ax2 = axarr[0].twinx()
+    gc = ax2.plot(dfs['count'], '--', linewidth=1, label='GOSAT Count')
+    ax2.set_xticklabels([])
+    ax2.set_ylabel('Count')
+
+    axarr[1].plot(dfa.loc[:, SL[f'abs_755{pol}']].median(axis=1), 'g-')
+    axarr[1].fill_between(dfa.index,
+                          y1=dfa.loc[:, SL[f'abs_755{pol}']].min(axis=1),
+                          y2=dfa.loc[:, SL[f'abs_755{pol}']].max(axis=1),
+                          color='green', alpha=0.15)
+    axarr[1].hlines([0], xmin=dfa.index[0], xmax=dfa.index[-1],
+                    color='grey', linewidth=1.0, linestyle='dashed')
+    axarr[1].set_ylabel('SIF Anomaly\n[mW/m$^2$/sr/nm]')
+
+    fig.align_ylabels()
+
+    #fig.tight_layout()
+    fig.legend([sl[0], gc[0]], ['Solar-induced Fluorescence (SIF)',
+                                'GOSAT Measurement Count'],
+               loc='lower center', fontsize=7, bbox_to_anchor=(0.5, -0.01))
+
+    fig.subplots_adjust(bottom=0.2)
+    plt.savefig(f'SIF_overview{pol}.pdf', bbox_inches='tight')
+    plt.close('all')
+
+aux_list = ['gome2_sif',
+            'spei',
+            'trmm_precip',
+            #######
+            'myd13_evi',
+            'fvc',
+            'lai',
+            #######
+            'myd11_lst_day',
+            'sm_passive',
+            'sm_active',
+            #####
+            'CASA_GEE', 'CASA_NEE', 'uoe-7.1b']
+
+aux_labels = ['GOME-2 SIF [mW/m$^2$/sr/nm]',
+              'SPEI',
+              'TRMM [mm/hr]',
+              ####
               'EVI',
-              #'Albedo 755nm',
-              'TRMM [mm/day]',
-              'GRACE [?]',
-              'LST [K]',
-              'Combined SM [m$^3$/m$^3$]',
-              'FVC [?]',
+              'FVC',
               'LAI',
+              ####
+              'LST [K]',
+              'Passive SM [m$^3$/m$^3$]',
+              'Active SM [%]',
+              ####
               'CASA GPP [gC/m$^2$/yr]',
-              'CASA NPP [gC/m$^2$/yr]']
+              'CASA NPP [gC/m$^2$/yr]',
+              'UoE NPP [gC/m$^2$/yr]']
 
-ts_overview(dfa, abs_755_list_P, aux_list,
-            aux_labels=aux_labels, ncol=2,
-            symm_lim=True, fname='anomalies_755_abs_P.png')
+for absrel, pol, window in itertools.product(
+        ['abs', 'rel'], ['_P', '_S', ''], ['755', '772']):
+    for x in range(4):
+        sl = slice(x*3, (x+1)*3)
+        ts_overview(dfa, SL[f'{absrel}_{window}{pol}'], aux_list[sl],
+                    aux_labels=aux_labels[sl],
+                    plot_sif=False, ncol=1, figsize=(4, 3.5), 
+                    symm_lim=True, fname=f'anomalies_{window}_{absrel}{pol}_{x}_{anomaly_stat}.pdf')
 
-ts_overview(dfa, abs_755_list_S, aux_list,
+ts_overview(dfa, SL['rel_755_P'], aux_list,
             aux_labels=aux_labels, ncol=2,
-            symm_lim=True, fname='anomalies_755_abs_S.png')
+            symm_lim=True, fname='anomalies_755_rel_P.pdf')
+ts_overview(dfa, SL['abs_755_P'], aux_list,
+            aux_labels=aux_labels, ncol=2,
+            symm_lim=True, fname='anomalies_755_abs_P.pdf')
+
+ts_overview(dfa, SL['abs_755_S'], aux_list,
+            aux_labels=aux_labels, ncol=2,
+            symm_lim=True, fname='anomalies_755_abs_S.pdf')
+
+# THIS GIVES BEST ANOMALY CORRELATIONS..
+ts_overview(dfa, SL['abs_755'], aux_list,
+            aux_labels=aux_labels, ncol=2,
+            symm_lim=True, fname='anomalies_755_abs.pdf')
+
 """
 
 
@@ -285,9 +434,11 @@ ts_overview(dfa, rel_772_list, aux_list, ncol=2,
 
 # Build a new DF for model analysis
 dfm = pd.DataFrame(index=dfs.index,
-                   columns=['sif',] + aux_list)
-dfm['sif'] = dfa.loc[:, abs_755_list].median(axis=1)
-for aux in aux_list:
+                   columns=['sif', 'albedo_1', 'SZA'] + aux_list)
+dfm['sif'] = dfa.loc[:, SL['abs_755']].median(axis=1)
+for aux in dfm.columns:
+    if aux == 'sif':
+        continue
     dfm.loc[:, aux] = dfa.loc[:, aux].values
 
 
--- subset_fluorescence.py
+++ subset_fluorescence.py
@@ -16,14 +16,22 @@ h5_P = h5py.File('/Users/petersomkuti/Work/fluorescence/fluor_concatenated_P.h5'
 
 intersect = np.intersect1d(h5_S['Exposure_id'][:],
                            h5_P['Exposure_id'][:])
+union = np.union1d(h5_S['Exposure_id'][:],
+                   h5_P['Exposure_id'][:])
 
 print("{:d} common soundings.".format(len(intersect)))
+print("{:d} union soundings.".format(len(union)))
 
-srt_S = np.searchsorted(h5_S['Exposure_id'][:], intersect)
-srt_P = np.searchsorted(h5_P['Exposure_id'][:], intersect)
+srt_S = np.searchsorted(union, h5_S['Exposure_id'][:])
+srt_P = np.searchsorted(union, h5_P['Exposure_id'][:])
 
-lon = h5_S['lon'][:][srt_S]
-lat = h5_S['lat'][:][srt_S]
+lon = np.zeros(len(union))
+lat = np.zeros(len(union))
+
+lon[srt_S] = h5_S['lon'][:]
+lat[srt_S] = h5_S['lat'][:]
+lon[srt_P] = h5_P['lon'][:]
+lat[srt_P] = h5_P['lat'][:]
 
 subset_idx = np.where((lon >= lon_min) & (lon <= lon_max) &
                       (lat >= lat_min) & (lat <= lat_max))[0]
@@ -34,8 +42,6 @@ skip_list = ['a_CO2', 'a_CH4', 'a_O2', 'a_H2O', 'a_aero1', 'a_aero1_tot',
              'a_ic', 'a_ic_tot', 'a_strat', 'a_strat_tot',
              'r_akfull_755', 'r_akfull_772',
              'r_pwgts_755', 'r_pwgts_772']
-
-subset_list_S = srt_S[subset_idx].tolist()
 
 with h5py.File('fluorescence_subset.h5', 'w') as h5_out:
 
@@ -49,16 +55,43 @@ with h5py.File('fluorescence_subset.h5', 'w') as h5_out:
         
         print(key)
         if (h5_S[key].shape[0] == len(h5_S['Exposure_id'])):
+            
+            if ('corr' in key) or (key[:2] == 'r_') or (key == 'qual_flag'):                
+                this_key_S = key + '_S'
+                this_key_P = key + '_P'
+                this_data_S = np.zeros(len(union), dtype=h5_S[key].dtype)
+                this_data_P = np.zeros(len(union), dtype=h5_S[key].dtype)
+                this_data_S[:] = np.nan
+                this_data_P[:] = np.nan
+                
 
-            if 'corr' in key:
-                h5_out.create_dataset(key+'_S', data=h5_S[key][:][srt_S[subset_idx]])
-                h5_out.create_dataset(key+'_P', data=h5_P[key][:][srt_P[subset_idx]])
+                this_data_S[srt_S] = h5_S[key][:]
+                this_data_P[srt_P] = h5_P[key][:]
+
+                h5_out.create_dataset(this_key_S, data=this_data_S[subset_idx])
+                h5_out.create_dataset(this_key_P, data=this_data_P[subset_idx])
+                print(f"Written {key}'s")
+                
             else:
-                h5_out.create_dataset(key, data=h5_S[key][:][srt_S[subset_idx]],
-                                      compression=8)
+                if len(h5_S[key].shape) != 1:
+                    this_data = np.zeros((len(union),) + h5_S[key].shape[1:],
+                                         dtype=h5_S[key].dtype)
+                else:
+                    this_data = np.zeros(len(union), dtype=h5_S[key].dtype)
+                try:
+                    this_data[:] = np.nan
+                except:
+                    print(f"Could not create NaNs for {key}.")
+                    
+                this_data[srt_S] = h5_S[key][:]
+                this_data[srt_P] = h5_P[key][:]
+
+                h5_out.create_dataset(key, data=this_data[subset_idx])
+                print(f"Written {key}")
+
         else:
             print('..special key')
-            h5_out.create_dataset(key, data=h5_S[key][:],
-                                  compression=8)
+            #h5_out.create_dataset(key, data=h5_S[key][:],
+            #                      compression=8)
 
         
